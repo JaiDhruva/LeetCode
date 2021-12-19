4822f5d96d968794337b7613c19e9e5ef2992c44
dhruva <jaidhruv510@gmail.com>
2021-12-19 16:48:28 +0530

    fix folder structure

--- TwoSum/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-var twoSumCalculator = new TwoSum.TwoSumCalculator();
-var a = twoSumCalculator.GetTwoSum(new int[5] { 1,2,3,4,5},7);
