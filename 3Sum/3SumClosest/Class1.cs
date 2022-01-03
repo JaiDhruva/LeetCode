ce021de5b4ffa56fa1ab2113f3cdc40fb1027d3b
dhruva <jaidhruv510@gmail.com>
2022-01-03 10:26:26 +0530

    add three sum closest

---
+++ 3Sum/3SumClosest/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace _3SumClosest
+{
+    public class ThreeSumClosestCalculator
+    {
+        public int GetThreeSumClosest(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            int closest = nums[0] + nums[1] + nums[nums.Length - 1];
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                int j = i + 1;
+                int k = nums.Length - 1;
+
+                while (j < k)
+                {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum > target)
+                        k--;
+                    else
+                        j++;
+
+                    if (Math.Abs(sum - target) < Math.Abs(closest - target))
+                        closest = sum;
+                }
+            }
+            return closest;
+        }
+    }
+}
---
+++ 3Sum/ThreeSumTests/3SumClosestTests.cs
@@ -0,0 +1,41 @@
+﻿using _3SumClosest;
+using ThreeSum;
+using Xunit;
+
+namespace ThreeSumTests
+{
+    public class ThreeSumClosestTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var sumCalculator = new ThreeSumClosestCalculator();
+            var response = sumCalculator.GetThreeSumClosest(new int[] { -1, 2, 1, -4 }, 1);
+            Assert.Equal(2, response);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var sumCalculator = new ThreeSumClosestCalculator();
+            var response = sumCalculator.GetThreeSumClosest(new int[] { 1, 1, 1, 0 }, -100);
+            Assert.Equal(2, response);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var sumCalculator = new ThreeSumClosestCalculator();
+            var response = sumCalculator.GetThreeSumClosest(new int[] { 1, 1, -1, -1, 3 }, -1);
+            Assert.Equal(-1, response);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var sumCalculator = new ThreeSumClosestCalculator();
+            var response = sumCalculator.GetThreeSumClosest(new int[] { 0, 2, 1, -3 }, 1);
+            Assert.Equal(0, response);
+        }
+    }
+}
